9d40de80cbd66cbeefe00f5f95b696aa5d74075e
janakaprageeth <janakaprageeth99@gmail.com>
2023-07-28 22:29:25 +0530

    new project

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '7ba2a367e933911a8edec0a3243f00bf'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
